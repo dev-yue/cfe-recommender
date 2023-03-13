f856b971d37dc93ecf817e7a3dfaabaa8bdf1a6d
Adamyuyue <67887367+Adamyuyue@users.noreply.github.com>
2023-03-12 22:43:34 -0500

    7-add-Dynamic-Django-with-HTMX

--- src/cfehome/celery.py
+++ src/cfehome/celery.py
@@ -2,19 +2,24 @@ import os
 
 from celery import Celery
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cfehome.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cfehome.settings')
 
 app = Celery('cfehome')
 
 # CELERY_
-app.config_from_object("django.conf:settings", namespace="CELERY")
+app.config_from_object("django.conf:settings", namespace='CELERY')
 
-
+# app.conf.broker_url = ''
+# app.conf.result_backend ='django-db'
 app.autodiscover_tasks()
 
 app.conf.beat_schedule = {
     "run_movie_rating_avg_every_30": {
-        'task': 'task_calculate_movie_ratings',
-        'schedule': 60, # 30 min,
+        'task': 'task_update_movie_ratings',
+        'schedule': 60 * 30, # 30 min,
+    },
+     "run_rating_export_every_hour": {
+        'task': 'export_rating_dataset',
+        'schedule': 60 * 60, # 1 hr,
     }
 }
